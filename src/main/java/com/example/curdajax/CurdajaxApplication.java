ef36d904b6be68882c8ca8ad34ee8f12c6b72686
kant300 <p.o.i.u.0987552@gmail.com>
2026-03-03 18:12:32 +0900

    Initial commit

---
+++ src/main/java/com/example/curdajax/CurdajaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.curdajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdajaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurdajaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/curdajax/CurdajaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curdajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdajaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
